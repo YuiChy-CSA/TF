e465a8deb38e7ab5d86687c129bd83587075aecb
Yui Chy <49412868+YuiChy-CSA@users.noreply.github.com>
2024-05-02 20:48:50 +0700

    Update TF_keys.js

--- TF_keys.js
+++ TF_keys.js
@@ -1,34 +1,33 @@
 /*
 Script Author: Yui Chy
 Source: https://gitlab.com/lodepuly/vpn_tool/-/raw/master/Resource/Script/TestFlight/TF_keys.js
+Source https://raw.githubusercontent.com/manhseo/TF_Trap/main/QX/TF_keys.js
 */
 
 const reg1 = /^https:\/\/testflight\.apple\.com\/v3\/accounts\/(.*)\/apps$/;
 const reg2 = /^https:\/\/testflight\.apple\.com\/join\/(.*)/;
-if (reg1.test($request.url)) {
-    $persistentStore.write(null, 'request_id')
-    let url = $request.url
-    let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2')
-    let session_id = $request.headers['X-Session-Id'] || $request.headers['x-session-id']
-    let session_digest = $request.headers['X-Session-Digest'] || $request.headers['x-session-digest']
-    let request_id = $request.headers['X-Request-Id'] || $request.headers['x-request-id']
-    let ua = $request.headers['User-Agent'] || $request.headers['user-agent']
-    $persistentStore.write(key, 'key')
-    $persistentStore.write(session_id, 'session_id')
-    $persistentStore.write(session_digest, 'session_digest')
-    $persistentStore.write(request_id, 'request_id')
-    $persistentStore.write(ua, 'tf_ua')
-    console.log($request.headers)
-    if ($persistentStore.read('request_id') !== null) {
-      $notification.post('Thông tin TF được truy xuất', 'Truy xuất thông tin thành công, vui lòng đóng script!','')
 
-    } else {
-      $notification.post('Thông tin TF không được truy xuất','Truy xuất thông tin thất bại, vui lòng bật công tắc Mitm over HTTP2 và khởi động lại VPN và ứng dụng TestFlight!','')
-    }
-    $done({})
-}
-if (reg2.test($request.url)) {
-  let appId = $persistentStore.read("APP_ID");
+if (reg1.test($request.url)) {
+  $prefs.setValueForKey(null, "request_id");
+  let url = $request.url;
+  let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, "$2");
+  const headers = Object.keys($request.headers).reduce((t, i) => ((t[i.toLowerCase()] = $request.headers[i]), t), {});
+
+  let session_id = headers["x-session-id"];
+  let session_digest = headers["x-session-digest"];
+  let request_id = headers["x-request-id"];
+  $prefs.setValueForKey(key, "key");
+  $prefs.setValueForKey(session_id, "session_id");
+  $prefs.setValueForKey(session_digest, "session_digest");
+  $prefs.setValueForKey(request_id, "request_id");
+  if ($prefs.valueForKey("request_id") !== null) {
+    $notify("Tự động tham gia TestFlight", "Lấy thông tin thành công", "");
+  } else {
+    $notify("Tự động tham gia TestFlight", "Không thể lấy thông tin", "Vui lòng thêm testflight.apple.com vào danh sách cho phép");
+  }
+  $done({});
+} else if (reg2.test($request.url)) {
+  let appId = $prefs.valueForKey("APP_ID");
   if (!appId) {
     appId = "";
   }
@@ -39,10 +38,11 @@ if (reg2.test($request.url)) {
   if (arr.length > 0) {
     appId = arr.join(",");
   }
-  $persistentStore.write(appId, "APP_ID");
-  $notification.post("TestFlight tự động tham gia", `Đã thêm APP_ID: ${id}`, `ID hiện tại: ${appId}`);
-  $done({})
+  $prefs.setValueForKey(appId, "APP_ID");
+  $notify("Tự động tham gia TestFlight", `Đã thêm APP_ID: ${id}`, `ID hiện tại: ${appId}`);
+  $done({});
 }
+
 function unique(arr) {
   return Array.from(new Set(arr));
 }
